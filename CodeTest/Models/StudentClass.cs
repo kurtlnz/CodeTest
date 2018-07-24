6d3691bafb20c5c70c659f2c0451b26c4c5bfb4e
Kurt <klim049@gmail.com>
2018-07-24 22:16:30 +1000

    Update Index view to include table sorting

--- CodeTest/Models/StudentClass.cs
+++ CodeTest/Models/StudentClass.cs
@@ -8,7 +8,7 @@ namespace CodeTest.Models
     public class StudentClass
     {
         public int StudentClassId { get; set; }
-        public int StudentId { get; set; }
-        public int ClassId { get; set; }
+        public Student Student { get; set; }
+        public Class Class { get; set; }
     }
 }
